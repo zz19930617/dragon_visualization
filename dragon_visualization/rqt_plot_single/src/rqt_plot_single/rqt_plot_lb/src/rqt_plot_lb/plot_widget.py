bea324a697969e59dd849de8f8c0618afb62407b
zhang <2673549804@qq.com>
2017-04-13 16:39:14 +0800

    change the font for big screen

--- dragon_visualization/rqt_plot_single/src/rqt_plot_single/rqt_plot_lb/src/rqt_plot_lb/plot_widget.py
+++ dragon_visualization/rqt_plot_single/src/rqt_plot_single/rqt_plot_lb/src/rqt_plot_lb/plot_widget.py
@@ -31,7 +31,7 @@ class PlotWidget(QWidget):
         
         #ui
         rp = rospkg.RosPack()
-        ui_file = os.path.join('/home/robot/catkin_ws/src/dragon_visualization/rqt_plot_single/src/rqt_plot_single/rqt_plot_lb', 'resource' , 'plot.ui')
+        ui_file = os.path.join('/home/zhangzhi/catkin_ws/src/dragon_robot/dragon_visualization/rqt_plot_single/src/rqt_plot_single/rqt_plot_lb', 'resource' , 'plot.ui')
         loadUi(ui_file , self)
         
         #subscribe
